7dbbd80952a1b5721e7fa890de84c4f80853e311
Odorico Veloso <odoricoveloso@gmail.com>
2025-06-05 15:21:55 -0300

    Update run_tor_proxy.sh

--- extra/run_tor_proxy.sh
+++ extra/run_tor_proxy.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-sleep 300 # Espera 5 minutos antes de iniciar o Tor
-
-# Inicia o Tor como um processo em segundo plano
-
-/app/tor/bin/run_tor_proxy &
-
-# Aguarda o Tor iniciar
-sleep 300 # Espera 5 minutos para garantir que o Tor esteja totalmente iniciado
-
-# Loop para enviar NEWNYM a cada 2 minutos
-while true; do
-  # Envia o comando NEWNYM para a porta de controle do Tor
-  echo -e "AUTHENTICATE \"torProxy@123\"\r\nSIGNAL NEWNYM\r\nQUIT\r\n" | nc 127.0.0.1 9051
-  sleep 300 # Espera 5 minutos antes de enviar o próximo NEWNYM
-  echo "Sent NEWNYM signal to Tor"
-done
+/app/tor/bin/run_tor_prox
